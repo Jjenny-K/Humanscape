989da6c36293565865da2a57f4cf26dc7c95013e
woodstock1993 <diqkqkqk@gmail.com>
2022-05-11 14:18:22 +0900

    (add) deplot test

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from studies.views import testView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('studies/v1/', include('studies.urls')),
+    path('studies/v1/', testView.as_view()),
 ]
