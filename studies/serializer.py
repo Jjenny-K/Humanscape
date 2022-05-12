4678a1a9decf32ec74c4b2ebe9458a3586d3e2c9
Jjenny-K <kkjjh0129@gmail.com>
2022-05-12 11:25:03 +0900

    (fix)serializer.py 수정

--- studies/serializer.py
+++ studies/serializer.py
@@ -3,8 +3,10 @@ from studies.models import Study
 
 
 class StudySerializers(serializers.ModelSerializer):
+    institute = serializers.ReadOnlyField(source='institute.name')
+    department = serializers.ReadOnlyField(source='institute.department')
+
     class Meta:
         model = Study
-        fields = ['title', 'number', 'period', 'scope', 'category', 'stage', \
-             'total_target', 'institute', 'department']
-        
+        fields = ('title', 'number', 'period', 'scope', 'category', 'stage',
+                  'total_target', 'institute', 'department')
