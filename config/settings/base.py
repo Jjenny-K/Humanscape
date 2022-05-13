268e81160df248ff457d21869c48c2b72b82ffe0
woodstock1993 <diqkqkqk@gmail.com>
2022-05-13 11:21:11 +0900

    (fix) 배포 설정 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -89,30 +89,6 @@ TEMPLATES = [
 ]
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASS'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -1,5 +1,7 @@
 from .base import *
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
