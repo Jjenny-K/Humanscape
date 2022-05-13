fe553904d34fd69c48705d1fb8951a0c2c332555
Jjenny-K <kkjjh0129@gmail.com>
2022-05-13 12:41:28 +0900

    (fix)trailing slash 제거

--- studies/urls.py
+++ studies/urls.py
@@ -6,6 +6,6 @@ from studies import views
 """
 
 urlpatterns = [
-    path('studies/', views.StudyList.as_view(), name='studylist'),
+    path('studies', views.StudyList.as_view(), name='studylist'),
     path('studies/<str:pk>', views.StudyRetrieve.as_view(), name='studyretrieve')
 ]
